85b7c4cee895cc41efc9117e3827e1c09548ef22
yasufumi <yasufumi.taniguchi@gmail.com>
2018-12-31 00:18:32 +0900

    update version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 
 setup(
     name='pipelib',
-    version='0.1.3',
+    version='0.1.4',
     description='pipeline architecture data library',
     url='https://github.com/yasufumy/pipelib',
     author='Yasufumi Taniguchi',
