81eeeb3a1f958e9984eb7c7a6e92413202739a90
yasufumi <yasufumi.taniguchi@gmail.com>
2018-12-30 22:49:00 +0900

    update numpy_array.py

--- examples/numpy_array.py
+++ examples/numpy_array.py
@@ -10,5 +10,5 @@ from pipelib import Dataset
 if __name__ == '__main__':
     data = Dataset(range(100))
 
-    print(data.map(partial(np.array, np.int32)))
-    print(data.map(partial(np.array, np.float32)))
+    print(data.map(partial(np.array, dtype=np.int32)).take(5))
+    print(data.map(partial(np.array, dtype=np.float32)).take(5))
