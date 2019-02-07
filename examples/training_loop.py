2de9ccce1e89b604977709adcb0ace293d2e6cdc
yasufumi <yasufumi.taniguchi@gmail.com>
2019-02-07 17:00:23 +0900

    fix bug in training_loop.py

--- examples/training_loop.py
+++ examples/training_loop.py
@@ -45,9 +45,9 @@ def do_something(batch):
 if __name__ == '__main__':
 
     # data preparation
-    if not osp.exists('train.en'):
+    if not osp.exists('test.en'):
         os.system('curl -sO https://raw.githubusercontent.com/odashi/small_parallel_enja/master/test.en')
-    if not osp.exists('train.ja'):
+    if not osp.exists('test.ja'):
         os.system('curl -sO https://raw.githubusercontent.com/odashi/small_parallel_enja/master/test.ja')
 
     en = prepare_data('test.en', 'en.processed')
