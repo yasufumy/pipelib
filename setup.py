a34e124865708799778634829f62c4ad6fb18ea1
yasufumi <yasufumi.taniguchi@gmail.com>
2019-02-10 20:41:36 +0900

    update the version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 
 setup(
     name='pipelib',
-    version='0.2.1',
+    version='0.2.2',
     description='pipeline architecture data library',
     long_description=open('./README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
